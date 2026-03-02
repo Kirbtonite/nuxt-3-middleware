36d52cc36ba3fb0995eefc6a175a4b9a8d9fc625
Jacky Southiphonh <southiphonhjacky@gmail.com>
2026-03-02 16:00:12 -0500

    lesson finished

--- middleware/01.setup.global.js
+++ middleware/01.setup.global.js
@@ -1,3 +1,16 @@
-export default defineNuxtRouteMiddleware((to, from) => {
+export default defineNuxtRouteMiddleware(async (to, from) => {
   console.log('--- Setup Global Middleware ---')
+  if (to.path === '/login') {
+    throw createError({
+      statusCode: 405,
+      statusMessage: 'Oh snap, it broke!'
+    })
+  }
+  // return abortNavigation('Failed at global Middleware')
+  // try {
+  //   const data = await fetch('api/this-path')
+  //   return navigateTo('/profile' + data.user.name)
+  // } catch (error) {
+  //   return abortNavigation(error)
+  // }
 })
