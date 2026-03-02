dc20958888bcf5d63f74d24900b28cff3f128b5b
Jacky Southiphonh <southiphonhjacky@gmail.com>
2026-03-02 10:54:23 -0500

    lesson finished

--- pages/logout.vue
+++ pages/logout.vue
@@ -1,4 +1,13 @@
-<script setup></script>
+<script setup>
+definePageMeta({
+  middleware: function (to, from) {
+    const isAuthenticated = useCookie('is-authenticated')
+    const currentUser = useCookie('current-user')
+    isAuthenticated.value = ''
+    currentUser.value = ''
+  }
+})
+</script>
 
 <template>
   <article class="grid">
